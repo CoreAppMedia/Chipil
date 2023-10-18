5e16854f3b687b50e96e32d04464a7a779d16364
CoreAppMedia <CoreAppMedia@gmail.com>
2023-10-17 18:38:07 -0600

    Initial Commit

---
+++ Chipil/ChipilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipilApp.swift
+//  Chipil
+//
+//  Created by mac20@ioslabacatlan.appleid.com on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChipilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chipil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chipil
+//
+//  Created by mac20@ioslabacatlan.appleid.com on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
